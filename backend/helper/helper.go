b2ec879eb8d67d00fb8d9127cdf197c01b39ccc5
kanaami <knym4a.r0613@gmail.com>
2026-02-11 23:32:10 +0900

    lint: Encodeのエラー戻り値が無視されていたので修正

--- backend/helper/helper.go
+++ backend/helper/helper.go
@@ -9,7 +9,9 @@ import (
 func JsonResponse(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 // ヘルパー: エラーレスポンス
